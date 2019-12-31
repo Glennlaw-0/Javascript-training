b7efc92fc41f8717ff44734919bedc839a08e773
Glenn Law <Glennlaw@me.com>
2019-12-31 17:38:00 -0500

    part 2 of timestamps & comparisons added

--- Chapter 11/sandbox.js
+++ Chapter 11/sandbox.js
@@ -4,28 +4,45 @@
 // objects directly
 
 //Date constructor to get all date
+// const now = new Date();
+
+// console.log(now);
+// console.log(typeof now);
+
+// // year, months, day, times
+// console.log('getFullYear:', now.getFullYear());
+// console.log('getMonth:', now. getMonth());
+// console.log('getDate:', now.getDate());
+// console.log('getDay:', now.getDay());
+// console.log('getHours:', now.getHours());
+// console.log('getMinutes:', now.getMinutes());
+// console.log('getSeconds:', now.getSeconds());
+
+// //timestamp
+
+// console.log('timestamp:', now.getTime());
+
+// //to date string
+
+// console.log(now.toDateString());
+// console.log(now.toTimeString());
+// console.log(now.toLocaleString());
+// console.log(now.toLocaleDateString());
+// console.log(now.toLocaleTimeString());
+
+//part 2 timestamps & comparisons
+
+const before = new Date('February 1 2019 7:30:59');
 const now = new Date();
 
-console.log(now);
-console.log(typeof now);
+// console.log(now.getTime(), before.getTime());
 
-// year, months, day, times
-console.log('getFullYear:', now.getFullYear());
-console.log('getMonth:', now.getMonth());
-console.log('getDate:', now.getDate());
-console.log('getDay:', now.getDay());
-console.log('getHours:', now.getHours());
-console.log('getMinutes:', now.getMinutes());
-console.log('getSeconds:', now.getSeconds());
+const diff = now.getTime() - before.getTime();
 
-//timestamp
+console.log(diff);
 
-console.log('timestamp:', now.getTime());
+const mins = Math.round(diff / 1000 / 60);
+const hours = Math.round(mins / 60);
+const days = Math.round(hours / 24);
 
-//to date string
-
-console.log(now.toDateString());
-console.log(now.toTimeString());
-console.log(now.toLocaleString());
-console.log(now.toLocaleDateString());
-console.log(now.toLocaleTimeString());
+console.log(days);
